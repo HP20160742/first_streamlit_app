5ed7d05ffed37aa99aea8ca3193acfdc96e2219b
HP20160742 <122446657+HP20160742@users.noreply.github.com>
2023-01-13 14:54:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ import streamlit
 #streamlit.text('Hiking')
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🧋Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥚Hard-Boiled Free-Range Egg')
