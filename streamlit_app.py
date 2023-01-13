e111322b29a67ca3c27b34eef7c53f79ed17e738
HP20160742 <122446657+HP20160742@users.noreply.github.com>
2023-01-13 14:45:27 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
-import streamlit
-streamlit.title('Hello, Harish here..')
-streamlit.header('List of My Hobbies:')
-streamlit.text('Playing Cricket')
-streamlit.text('Hiking')
+#import streamlit
+#streamlit.title('Hello, Harish here..')
+#streamlit.header('List of My Hobbies:')
+#streamlit.text('Playing Cricket')
+#streamlit.text('Hiking')
+#
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
